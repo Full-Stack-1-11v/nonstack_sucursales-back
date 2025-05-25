356883bac543b3b301ac52e5fe442b5d7ec840ea
Your Name <you@example.com>
2025-05-25 00:42:58 -0400

    update controller imports

--- microservicio-sucursales/src/main/java/com/perfulandia/cl/microservicio_sucursales/controller/CiudadController.java
+++ microservicio-sucursales/src/main/java/com/perfulandia/cl/microservicio_sucursales/controller/CiudadController.java
@@ -2,20 +2,19 @@ package com.perfulandia.cl.microservicio_sucursales.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.perfulandia.cl.microservicio_sucursales.service.CiudadService;
 import com.perfulandia.cl.microservicio_sucursales.model.Ciudad;
+import com.perfulandia.cl.microservicio_sucursales.service.CiudadService;
 
 
 @RestController
