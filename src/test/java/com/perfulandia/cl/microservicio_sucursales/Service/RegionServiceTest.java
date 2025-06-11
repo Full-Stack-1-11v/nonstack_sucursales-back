c946f7899e22bef5b6271708d27d4c09db3e3420
Your Name <you@example.com>
2025-06-10 21:56:20 -0400

    update a regionservice test

--- src/test/java/com/perfulandia/cl/microservicio_sucursales/Service/RegionServiceTest.java
+++ src/test/java/com/perfulandia/cl/microservicio_sucursales/Service/RegionServiceTest.java
@@ -1,8 +1,9 @@
-package com.perfulandia.cl.microservicio_sucursales;
+package com.perfulandia.cl.microservicio_sucursales.Service;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
 
 
 import java.util.List;
@@ -47,13 +48,44 @@ public class RegionServiceTest {
 
     @Test
     public void testSaveRegion() {
-        //Given
-        Region regionTest = regionRepository.findByNombreRegion("Region prueba 1");
-        
 
+        // Given
+        Region region = new Region(1, "Region prueba 1");
+        when(regionRepository.save(region)).thenReturn(region);
+
+        Region savedRegion = regionService.saveRegion(region);
+        assertNotNull(savedRegion);
+        assertEquals(1, savedRegion.getIdRegion());
+
+
+    }
+
+    @Test
+    public void testDeleteById() {
+
+        // Given
+        Integer id = 1;
+        doNothing().when(regionRepository).deleteById(id);
+
+        regionService.deleteById(id);
+        verify(regionRepository, times(1)).deleteById(id);
         
     }
 
+    @Test
+    public void testGetRegionById() {
+        // Given
+        Integer id = 1;
+        Region expectedRegion = new Region(1, "Region existente 1");
+        when(regionRepository.findByIdRegion(id)).thenReturn(expectedRegion);
+
+        Region result = regionService.getRegionById(id);
+        assertNotNull(result);
+        assertEquals(expectedRegion.getIdRegion(), result.getIdRegion());
+                
+    }
+
+   
 
 
 // >>>> ./mvnw clean verify para ejecutar los test en la terminal
